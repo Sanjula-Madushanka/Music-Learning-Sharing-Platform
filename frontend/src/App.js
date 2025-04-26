82a9a06be7d93e93fd27128d3abb2ef37bd86dd4
NimeshaShehan <nimeshashehan412@gmail.com>
2025-04-26 23:27:44 +0530

    update App.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,14 +9,16 @@ import Favorites from './components/favorites';
 function App() {
   return (
     <Router>
-      <Navbar />
-      <div className="container mt-4">
-        <Routes>
-          <Route path="/" element={<MusicFeed />} />
-          <Route path="/music/:id" element={<MusicDetail />} />
-          <Route path="/upload" element={<UploadMusic />} />
-          <Route path="/favorites" element={<Favorites />} />
-        </Routes>
+      <div className="min-h-screen bg-gray-900 text-white">
+        <Navbar />
+        <div className="container mx-auto mt-4 p-4">
+          <Routes>
+            <Route path="/" element={<MusicFeed />} />
+            <Route path="/music/:id" element={<MusicDetail />} />
+            <Route path="/upload" element={<UploadMusic />} />
+            <Route path="/favorites" element={<Favorites />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
