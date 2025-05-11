30dfa1784ca6da91d75aa876fbc10dfa43b15e0b
MaryChris21 <marysechristina104@gmail.com>
2025-05-11 14:01:31 +0530

    App.jsx update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,28 @@
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-import { useEffect, useState } from "react";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import Profile from "./pages/Profile";
-import EditProfile from "./pages/EditProfile";
-import UsersList from "./pages/UserList";
-import Navbar from "./components/Navbar";
-import UserProfile from "./pages/UserProfile";
-import OAuthRedirect from "./pages/OAuthRedirect";
-import CompleteProfile from "./pages/CompleteProfile";
+"use client"
+
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom"
+import { useEffect, useState } from "react"
+import Register from "./pages/Register"
+import Login from "./pages/Login"
+import Profile from "./pages/Profile"
+import EditProfile from "./pages/EditProfile"
+import UsersList from "./pages/UserList"
+import Navbar from "./components/Navbar"
+import UserProfile from "./pages/UserProfile"
+import OAuthRedirect from "./pages/OAuthRedirect"
+import CompleteProfile from "./pages/CompleteProfile"
+import CreatePost from "./pages/CreatePost"
+import EditPost from "./pages/EditPost"
+import PostFeed from "./pages/PostFeed"
 
 const AppWrapper = () => {
-  const location = useLocation();
-  const [showNavbar, setShowNavbar] = useState(false);
+  const location = useLocation()
+  const [showNavbar, setShowNavbar] = useState(false)
 
   useEffect(() => {
-    const hiddenRoutes = ["/login", "/register", "/oauth-redirect", "/complete-profile"];
-    setShowNavbar(!hiddenRoutes.includes(location.pathname));
-  }, [location]);
+    const hiddenRoutes = ["/login", "/register", "/oauth-redirect", "/complete-profile"]
+    setShowNavbar(!hiddenRoutes.includes(location.pathname))
+  }, [location])
 
   return (
     <>
@@ -31,17 +36,20 @@ const AppWrapper = () => {
         <Route path="/profile" element={<UserProfile />} />
         <Route path="/oauth-redirect" element={<OAuthRedirect />} />
         <Route path="/complete-profile" element={<CompleteProfile />} />
+        <Route path="/create-post" element={<CreatePost />} />
+        <Route path="/edit-post/:id" element={<EditPost />} />
+        <Route path="/feed" element={<PostFeed />} />
       </Routes>
     </>
-  );
-};
+  )
+}
 
 function App() {
   return (
     <BrowserRouter>
       <AppWrapper />
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
